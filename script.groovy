cb02ef7c736b0dbfcc64ab8e693598e7c406050c
Devesh Kumawat <kumawatdevesh99@gmail.com>
2021-12-21 17:21:59 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def testApp() {
         usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')
     ]) {
         sh "docker build -t kumawatdevesh99/e-comm:latest ."
-        sh "echo $PASS | docker login -u $USER -password-stdin"
+        sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push kumawatdevesh99/e-comm:latest"
     }
 }
